60dbcec52bf33dd738bec231547ddb8728500218
Даник <lobunetsdaniil@yandex.ru>
2023-04-18 21:34:51 +0300

    game page dynamic menu added

---
+++ client/cw/components/overviewSection.tsx
@@ -0,0 +1,10 @@
+import {Flex} from "@chakra-ui/react";
+import React from "react";
+
+const OverviewSection = () => {
+  return (
+    <Flex color="white">dsshjgkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkl</Flex>
+  );
+};
+
+export default OverviewSection;
--- client/cw/src/pages/games/[id].tsx
+++ client/cw/src/pages/games/[id].tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useRef} from "react";
 import Layout from "../../../components/layout";
 import {Flex, Box} from "@chakra-ui/react";
+import OverviewSection from "../../../components/overviewSection";
 
 const Game = () => {
+  const linksBox = useRef<HTMLInputElement>(null);
+  const changeActiveLink = (e: any) => {
+    let linksArr: any = linksBox.current?.children;
+    for (let i = 0; i < linksArr!.length!; i++) {
+      linksArr![i].style!.color = "white";
+    }
+    e.target.style.color = "blue";
+  };
+
   return (
     <>
       <main>
         <Layout headerType="lkkl">
-          <Flex flex={1} w="100%" h="100%">
-            <Flex h="266px" bg="red" w="100%" direction="column">
+          <Flex flex={1} w="100%" h="100%" direction="column">
+            <Flex
+              h="266px"
+              bg="red"
+              w="100%"
+              direction="column"
+              justify="flex-end"
+            >
               <Flex gap="7px">
                 <Flex>adad</Flex>
                 <Flex>dada</Flex>
@@ -26,7 +42,19 @@ const Game = () => {
                   <Flex>jlkjkl</Flex>
                 </Flex>
               </Flex>
+              <Flex gap="10px" ref={linksBox}>
+                <Flex cursor="pointer" onClick={(e) => changeActiveLink(e)}>
+                  dadad
+                </Flex>
+                <Flex cursor="pointer" onClick={(e) => changeActiveLink(e)}>
+                  adada
+                </Flex>
+                <Flex cursor="pointer" onClick={(e) => changeActiveLink(e)}>
+                  dadad
+                </Flex>
+              </Flex>
             </Flex>
+            <OverviewSection />
           </Flex>
         </Layout>
       </main>
